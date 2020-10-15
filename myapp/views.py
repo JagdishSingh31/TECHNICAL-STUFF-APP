9b656241b619f1e14e1640ef8c73684d92756937
JagdishSingh <jagdish.singh119351@gmail.com>
2020-10-15 11:45:12 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,49 @@
+import requests
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+from . import models
+
+base_url = 'https://losangeles.craigslist.org/search/sss?query='
+img_url = 'https://images.craigslist.org/{}_300x300.jpg'
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    models.new_search.objects.create(search=search)
+    final_url = base_url.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_list = soup.find_all('li', {'class': 'result-row'})
+
+    final_post = []
+
+    for post in post_list:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_img = img_url.format(post_id)
+
+        else:
+            post_img = 'https://craigslist.org/images/peace.jpg'
+
+        final_post.append((post_title, post_url, post_price, post_img))
+
+
+    stuff_for_frontend = {
+        'search': search,
+        'final_post': final_post,
+    }
+    return render(request, "my_app/new_search.html", stuff_for_frontend)
